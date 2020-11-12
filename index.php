88b70c9244e07f2dc036cee19c3d48bd18c2692a
Dimasodiin <dimassodiin7777@gmail.com>
2020-11-12 21:16:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+   <title>Halaman Utama</title>
+</head>
+<body>
+<fieldset>
+
+    <h1>Halaman Utama</h1>
+    <hr>
+    <fieldset>
+    <a href="login.php"><button >Login</button></a>
+    <a href="admin.php"><button>Admin</button></a>
+    <a href="guru.php"><button>Guru</button></a>
+    <a href="siswa.php"><button>Siswa</button></a>
+    </fieldset>
+</fieldset>
+</body>
+</html>
